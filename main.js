9ff76ec9de5b752eb2a3a6071f8b709f8c982d39
unknown <drawingyouinink@gmail.com>
2021-07-30 21:45:20 +0300

    Add sizes with list rendering

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 0,
-            onSale: true
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
         }
     }
 })
