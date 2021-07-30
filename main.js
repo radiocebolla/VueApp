b55421d223d46bc33038912acd74b482a553ee63
unknown <drawingyouinink@gmail.com>
2021-07-30 21:09:14 +0300

    Add conditional rendering

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: ''
+            inventory: 0,
+            onSale: true
         }
     }
 })
